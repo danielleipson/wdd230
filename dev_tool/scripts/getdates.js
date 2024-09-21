decfb2f3ff51f45cb0ca8c65021df5405063cc4b
danielleipson <131837684+danielleipson@users.noreply.github.com>
2024-09-21 15:05:40 -0600

    Delete dev_tool directory

--- dev_tool/scripts/getdates.js
+++
@@ -1,7 +0,0 @@
-//Last Modified Document Date
-const d = new Date(document.lastModified);
-document.getElementById("lastModified").innerHTML = d;
-
-//Copyright get Full Year
-const c = new Date();
-document.getElementById("fullYear").innerHTML = c.getFullYear();
