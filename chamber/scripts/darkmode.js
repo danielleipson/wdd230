050877ccb1f22434b11e9a3f5adfea3c79b2b3cb
danielleipson <danielleipson@gmail.com>
2024-11-02 23:43:19 -0600

    Week06 bug fixes

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,4 +1,25 @@
-const modeButton = document.querySelector("#switch");
+const toggleSwitch =
+    document.querySelector('.theme-slider input[type="checkbox"]');
+    
+    /* Function to change theme */
+    function switchTheme(e) {
+    
+    /* Once checkbox is checked default theme change to dark */
+    if (e.target.checked) {
+        document.documentElement.setAttribute('theme', 'dark');
+    }
+    
+    /* While page in dark mode and checkbox is 
+    checked then theme back to change light*/
+    else {
+        document.documentElement.setAttribute('theme', 'light');
+    }
+    }
+    
+    toggleSwitch.addEventListener('change', switchTheme, false);
+
+	
+/*const modeButton = document.querySelector("#switch");
 const main = document.querySelector("main");
 const body = document.querySelector("body");
 
@@ -16,4 +37,4 @@ modeButton.addEventListener("click", () => {
         body.style.color = "#000";
 		//modeButton.textContent = "🕶️";
 	}
-});
+});*/
