9526057a437708ad4f450539e30429553f47ba20
danielleipson <danielleipson@gmail.com>
2024-11-07 09:07:50 -0700

    week08 updates

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,6 +1,39 @@
-var temp= 57;
-var wSpeed= 15;
-var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+async function getWeather() {
+    let response = await fetch('https://api.weather.gov/gridpoints/PIH/121,70/forecast/hourly');
+    let data = await response.json();
+    return data
+}
 
-var windChill= Math.round(windChill);
-document.getElementById("windChill").innerHTML= windChill;
+
+let temp = 0;
+let windSpeed = ''
+
+
+getWeather().then(data => {
+    info = data.properties.periods[0]
+    console.log(data.properties.periods[0])
+    let windChill = '';
+
+    temp = info.temperature;
+    windSpeed = info.windSpeed;
+
+    let numberPattern = /\d+/g;
+    let actualWindSpeed = windSpeed.match(numberPattern);
+    console.log(actualWindSpeed)
+    windSpeed = actualWindSpeed[0];
+
+    const docuTemp = document.querySelector('#temp')
+    const docuWind = document.querySelector('#windspeed')
+    const docuChill = document.querySelector('#windchill')
+
+    docuTemp.innerHTML = `Temperature: ${temp}°F`
+    docuWind.innerHTML = `Wind: ${windSpeed} mph`
+
+    if (temp <= 50 && windSpeed > 3) {
+        windChill = (35.74 + 0.6215 * temp - 35.75(windSpeed ^ 0.16) + 0.4275 * temp * (windSpeed ^ 0.16))
+        docuChill.innerHTML = `Windchill: ${windChill} degrees`
+    } else {
+        docuChill.innerHTML = 'Windchill: N/A'
+    }
+
+});
--- lesson07/scripts/bom.js
+++ lesson07/scripts/bom.js
@@ -1,37 +1,49 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
-
 let chaptersArray = getChapterList() || [];
 
-chaptersArray.forEach(chapter => {diplayList(chapter)});
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
 
 button.addEventListener('click', () => {
     if (input.value !== '') {
-
         displayList(input.value);
         chaptersArray.push(input.value);
         setChapterList();
         input.value = '';
         input.focus();
+    } else {
+        input.focus()
     }
-});
+})
 
 function displayList(item) {
-       let li = document.createElement('li');
-       let deleteButton = document.createElement('button');
-        //const link = `https://www.churchofjesuschrist.org/search?facet=scriptures&lang=eng&query=${input.value}&page=1`
-       // li.innerHTML = `<a href=${link}>${input.value}</a>`;
-       li.textContent = item;
-       deleteButton.textContent = "❌";
-       // li.append(deleteButton);
-       // list.append(li);
-       // deleteButton.addEventListener('click', () => {
-        //    list.removeChild(li)
-      //  })
-       // input.focus();
-       // input.value = "";
-   // } else {
-   //     input.focus()
- //   }
-//})
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    li.textContent = item;
+    deleteButton.textContent = "❌";
+    deleteButton.classList.add('delete');
+    li.append(deleteButton);
+    list.append(li);
+    deleteButton.addEventListener('click', () => {
+        list.removeChild(li);
+        deleteChapter(li.textContent);
+        input.focus();
+    });
+}
+
+function setChapterList() {
+    localStorage.setItem('favBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('favBOMList'));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter((item) => item !== chapter);
+    setChapterList();
+}
